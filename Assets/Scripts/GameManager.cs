c7c2a3d38616d32a3abdc581506e68d071fd8d1a
LeDian01 <775340552@qq.com>
2019-12-12 15:23:20 +0800

    碰撞检测

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,11 +6,14 @@ public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
     GameObject wallPrefab;
+    GameObject Env;//环境物体的父节点
 
     Vector3 cornerPos;
     void Start()
     {
         wallPrefab = Resources.Load<GameObject>("Prefabs/Wall");
+
+        Env = GameObject.Find("Env");
         
         cornerPos = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Mathf.Abs(-Camera.main.transform.position.z)));//获取屏幕边界右上角坐标（2D可以忽略z值并设置为0）
         SetWall();
@@ -26,14 +29,38 @@ public class GameManager : MonoBehaviour
     {
         Quaternion r = Quaternion.Euler(0, 0, 0);
         Vector3[] wallPos = { new Vector3(0, cornerPos.y + 0.5f, 0), new Vector3(0, -cornerPos.y - 0.5f, 0), new Vector3(-cornerPos.x - 0.5f, 0, 0), new Vector3(cornerPos.x + 0.5f, 0, 0)};
+        List<GameObject> temp = new List<GameObject>();
+        for(int i = 0; i < 4; i++)
+        {
+            temp.Add( Instantiate(wallPrefab, wallPos[i], r));
+        }
 
-        var temp=Instantiate(wallPrefab,wallPos[0],r);
-        temp.transform.localScale = new Vector3(cornerPos.x * 2, 1, 1);
-        temp=Instantiate(wallPrefab,wallPos[1],r);
-        temp.transform.localScale = new Vector3(cornerPos.x * 2, 1, 1);
-        temp =Instantiate(wallPrefab,wallPos[2],r);
-        temp.transform.localScale = new Vector3(1, cornerPos.y * 2, 1);
-        temp =Instantiate(wallPrefab,wallPos[3],r);
-        temp.transform.localScale = new Vector3(1, cornerPos.y * 2, 1);
+
+        
+        temp[0].transform.localScale = new Vector3(cornerPos.x * 2, 1, 1);
+        temp[0].name = "NorthWall";
+        
+        temp[1].transform.localScale = new Vector3(cornerPos.x * 2, 1, 1);
+        temp[1].name = "SouthWall";
+        
+        temp[2].transform.localScale = new Vector3(1, cornerPos.y * 2, 1);
+        temp[2].name = "WestWall";
+        
+        temp[3].transform.localScale = new Vector3(1, cornerPos.y * 2, 1);
+        temp[3].name = "EastWall";
+
+        temp[0].transform.parent = Env.transform;
+        temp[1].transform.parent = Env.transform;
+        temp[2].transform.parent = Env.transform;
+        temp[3].transform.parent = Env.transform;
+
+        
+
     }
+
+    void Test()
+    {
+
+    }
+
 }
--- Assets/Scripts/SimpleMove.cs
+++ Assets/Scripts/SimpleMove.cs
@@ -11,17 +11,22 @@ public class SimpleMove : MonoBehaviour
     public float smooth;//转向速度
 
     public Text bugtext;//测试用
-    
+
+    public Vector3[] fang= {Vector3.up,Vector3.down,Vector3.left,Vector3.right,Vector3.zero };
+
     Vector3 direction;//当前移动方向
     Vector2 coordinate;//当前坐标
+    Vector3 prePosition;//上一帧的坐标。
+
 
     Quaternion rota;//目标朝向
-
     Queue<Vector3> directionQ;//输入接收
+
+    Rigidbody2D rb2d;
 
 
     bool hasChangeDirection;//判断是否可以改变方向
-    bool isStop;
+    HashSet<Vector3> stopSet;
 
     void Start()
     {
@@ -29,11 +34,16 @@ public class SimpleMove : MonoBehaviour
         smooth = 20;
 
         direction = Vector3.zero;
+        prePosition = transform.position;
         rota = Quaternion.Euler(0, 0, 0);
 
         hasChangeDirection = false;
-        isStop = false;
 
+        rb2d = GetComponent<Rigidbody2D>();
+
+        //顺序为上下左右
+
+        stopSet = new HashSet<Vector3>();
         directionQ = new Queue<Vector3>();
         transform.position = PointFix(transform.position);
     }
@@ -43,7 +53,8 @@ public class SimpleMove : MonoBehaviour
     {
         
         GetInput();//获取输入
-        Move();//移动
+        //Move();//移动
+        FMove();
         ToNextPoint();//判断是否到达下一格
 
 
@@ -59,17 +70,66 @@ public class SimpleMove : MonoBehaviour
         str.Clear();
         str.Append("当前方向： "+DtoD(direction)+'\n');
         if(directionQ.Count==0)
-        str.Append("下一个方向： 无");
+        str.Append("下一个方向： 无"+"\n");
         else
-        str.Append("下一个方向： "+DtoD(directionQ.Peek()));
-
-        str.Append('\n'+"IsStop: " +isStop+'\n');
+        str.Append("下一个方向： "+DtoD(directionQ.Peek())+"\n");
+        
+        foreach(var t in stopSet)
+        {
+            str.Append(t.ToString()+'\n');
+        }
 
         bugtext.text = str.ToString();
 
     }
-    void Move()
+
+    void FMove()
     {
+        if (IsOverHalf(transform.position, direction))
+        {
+            if (SetDirection())//设置方向
+            {
+                transform.position = PointFix(transform.position);
+            }
+        }//修改这个的逻辑！
+
+        prePosition = transform.position;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, rota, smooth * Time.deltaTime);//转向
+        rb2d.velocity = direction * speed;
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("碰撞发生");
+        direction = fang[4];
+
+        //transform.position = PointFix(transform.position);
+
+        foreach (var temp in collision.contacts)
+        {
+            stopSet.Add(Vector3Int.RoundToInt(Vector3.Normalize(temp.point - new Vector2(transform.position.x, transform.position.y))));
+        }
+
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        Debug.Log("持续碰撞");
+        ContactPoint2D con = collision.GetContact(0);
+
+        foreach (var temp in collision.contacts)
+        {
+            stopSet.Add(Vector3Int.RoundToInt(Vector3.Normalize(temp.point - new Vector2(transform.position.x, transform.position.y))));
+        }
+        
+        
+    }
+
+    void Move()//bug应该在这儿
+    {
+        if(direction==fang[4])
+            transform.position = PointFix(transform.position);
+
         //判断是否到达可位移点
         if (IsOverHalf(transform.position, direction))
         {
@@ -80,12 +140,14 @@ public class SimpleMove : MonoBehaviour
         }
 
         //移动玩家
-        if (!isStop) { 
-        transform.rotation = Quaternion.Lerp(transform.rotation, rota, smooth*Time.deltaTime);//转向
-        transform.position += direction * speed * Time.deltaTime;//位移
-        }
+        
+            CheckDirection();
+            transform.rotation = Quaternion.Lerp(transform.rotation, rota, smooth*Time.deltaTime);//转向
+        
+            transform.position += direction * speed * Time.deltaTime;//位移
+        
     }
-    Vector2 CheckPoint(Vector3 input)
+    Vector2 CheckPoint(Vector3 input)//返回目标所在整数节点，例如（0，0）至（1，1）这个矩形的坐标是（0，0）
     {
         Vector2 output = new Vector2(Mathf.FloorToInt(input[0]), Mathf.FloorToInt(input[1]));
         return output;
@@ -93,26 +155,25 @@ public class SimpleMove : MonoBehaviour
 
     void GetInput()
     {
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        if (!stopSet.Contains(fang[0]) && (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)))
         {
             directionQ.Enqueue(Vector3.up);
         }
 
-        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        if (!stopSet.Contains(fang[1]) && (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)))
         {
             directionQ.Enqueue(Vector3.down);
         }
 
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        if (!stopSet.Contains(fang[2]) && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)))
         {
             directionQ.Enqueue(Vector3.left);
         }
 
-        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        if (!stopSet.Contains(fang[3]) && (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)))
         {
             directionQ.Enqueue(Vector3.right);
         }
-        
     }
     //如果方向有改变，返回true
     bool SetDirection()
@@ -125,18 +186,25 @@ public class SimpleMove : MonoBehaviour
             {
                 directionQ.Dequeue();
             }//移除同向的输入。
-            isStop = false;
+           
+
             if (directionQ.Count != 0 && !hasChangeDirection)
             {
                 direction = directionQ.Dequeue();
-                isStop = false;
                 SetRotation();
-
                 hasChangeDirection = true;
                 return true;
             }
         }
         return false;
+    }
+
+    void CheckDirection()
+    {
+        if (stopSet.Contains(direction))
+        {
+            direction = fang[4];
+        }
     }
     bool ToNextPoint()//判断是否已经到达下一格
     {
@@ -211,7 +279,7 @@ public class SimpleMove : MonoBehaviour
         var re = new Vector3(Mathf.FloorToInt(input[0])+0.5f, Mathf.FloorToInt(input[1])+0.5f, input[2]);
         return re;
     }
-    void SetRotation()
+    void SetRotation()//旋转
     {
         if (direction == Vector3.up)
             rota = Quaternion.Euler(0, 0, 0);
@@ -223,20 +291,44 @@ public class SimpleMove : MonoBehaviour
             rota = Quaternion.Euler(0, 0, 270f);
     }
 
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        isStop = true;
-        direction = Vector3.zero;
-        transform.position = PointFix(transform.position);
+    //void OnCollisionEnter2D(Collision2D collision)//碰撞的执行顺序在 Update()之前
+    //{
 
-        directionQ.Clear();
-    }
-    void OnCollisionStay2D(Collision2D collision)
-    {
-        direction = Vector3.zero;
-        transform.position = PointFix(transform.position);
 
-        directionQ.Clear();
+    //    Debug.Log("碰撞发生");
+
+    //    transform.position = PointFix(transform.position);
+
+    //    ContactPoint2D con = collision.GetContact(0);
+    //    var t = new Vector3(con.point.x, con.point.y, 0) - transform.position;
+    //    t = Vector3Int.RoundToInt(t.normalized);
+    //    stopSet.Add(t);
+
+
+
+
+
+
+
+    //}
+    //void OnCollisionStay2D(Collision2D collision)
+    //{
+    //    Debug.Log("触发Stay");
+
+    //    ContactPoint2D con = collision.GetContact(0);
+    //    var t = new Vector3(con.point.x, con.point.y, 0) - transform.position;
+    //    t = Vector3Int.RoundToInt(t.normalized);
+    //    stopSet.Add(t);
+
+
+    //}
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        stopSet.Clear();
+
+        Debug.Log("碰撞结束");
+
+        //Debug.Log(con.point);
     }
 
 }
